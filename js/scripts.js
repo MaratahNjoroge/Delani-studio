6844b9b158d8a3866e3382b39fd51f3f43b55f5d
Maratah <maratah7@gmail.com>
2020-05-11 14:20:20 +0300

    modiefied the sripts.js file with inputs.

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+/*
+$(document).ready(function(){
+    $(".design1").click(function() {
+    $(".design1").toggle(500);
+    $(".details1").toggle(500);
+    });
+    $(".details1").click(function() {
+    $(".details1").toggle(500);
+    $(".design1").toggle(500);
+    });
+    $(".design2").click(function() {
+    $(".design2").toggle(500);
+    $(".details2").toggle(500);
+    });
+    $(".details2").click(function() {
+    $(".details2").toggle(500);
+    $(".design2").toggle(500);
+    });
+    $(".design3").click(function() {
+    $(".design3").toggle(500);
+    $(".details3").toggle(500);
+    });
+    $(".details3").click(function() {
+    $(".details3").toggle(500);
+    $(".design3").toggle(500);
+    });
+    });
+    */
+
+    $("p").click(function(){
+        $(this).hide();
+      });
